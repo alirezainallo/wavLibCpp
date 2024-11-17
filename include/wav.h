32f412712d211f515cdf4cd6c8ba27225e729224
aavanNira <alirezainallo@gmail.com>
2024-11-17 16:41:36 +0330

    windows usage

--- include/wav.h
+++ include/wav.h
@@ -7,7 +7,7 @@ enum {
 	WAV_DEVICE_SIPEED_CPP = 1, //1 //WAV_DEVICE_SIPEED_CPP
 	WAV_DEVICE_STM32      = 2, //2 //WAV_DEVICE_STM32
 };
-#define WAV_DEVICE 2 //WAV_DEVICE_STM32
+#define WAV_DEVICE 0 //WAV_DEVICE_PC_CPP
 
 
 #include <stdio.h>
